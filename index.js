ab4baf049e5c0acc2b9311f43e75ac4e8e8954c9
Moringa School <james.koipapi@student.moringaschool.com>
2022-10-04 16:06:14 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivernames, firstname){
+    const names = drivernames.filter(function(item){
+        return item.toLowerCase() === firstname.toLowerCase()
+
+
+    })
+    return names
+}
+function fuzzyMatch(drivernames, letters){
+    const filteredArray =drivernames.filter(function(names){
+        return names.substring(0,2) === letters
+    })
+    return filteredArray
+}
+function matchName(drivernames, names){
+    const filteredArray = drivernames.filter(function(list){
+      return list.name === names;
+    })
+    return filteredArray
+}
